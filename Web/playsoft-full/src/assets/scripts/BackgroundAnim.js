79150e629bb0a6325e0f361ec5bf5196bb10ae29
unknown <ricardo.ros.vq@gmail.com>
2023-05-10 21:00:20 +0200

    feature/background: fixed animation

--- Web/playsoft-full/src/assets/scripts/BackgroundAnim.js
+++ Web/playsoft-full/src/assets/scripts/BackgroundAnim.js
@@ -3,12 +3,12 @@ import anime from "animejs";
 export function anim(index) {
   var spans = document.querySelectorAll('.background-span')
   spans.forEach(span => {
-    span.style.backgroundColor = '#fff'
+    span.style.backgroundColor = '#0f0f0f'
   })
   anime({
     targets: ".background-span",
     scale: 0.95,
-    background: '#fff',
+    backgroundColor: '#fff',
     duration: 20,
     delay: anime.stagger(20, { grid: [18, 9], from: index }),
     endDelay: 50,
