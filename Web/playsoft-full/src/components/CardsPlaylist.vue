fef27e80866a73363efd9a521916a4c2391d5517
unknown <ricardo.ros.vq@gmail.com>
2023-06-04 21:17:41 +0200

    feature/musicPlayer: finished player

--- Web/playsoft-full/src/components/CardsPlaylist.vue
+++ Web/playsoft-full/src/components/CardsPlaylist.vue
@@ -19,7 +19,7 @@ export default {
     props: ["productItem", "PlayListsList"],
     data: function () {
         return {
-            pito: 'holaasdas'
+            
         }
     },
     components: { CreatePlaylistForm, BackGround },
